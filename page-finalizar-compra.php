549930fded426fc1c16cfdea0990d78ff431e3f1
rohyer <guilhermerl.dev@gmail.com>
2020-06-12 10:56:54 -0300

    Personalização da página carrinho

--- page-finalizar-compra.php
+++ page-finalizar-compra.php
@@ -1,19 +1,19 @@
 <?php get_header(); ?>
 
-    <div id="page-cart">
+    <div id="page-checkout">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <div class="title-pages">
-                        <h3>Carrinho</h3>
+                        <h3>Finalizar compra</h3>
                     </div>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-12">
-                    <div id="main-content-cart">
-                        <?php echo do_shortcode('[woocommerce_cart]'); ?>
+                    <div id="main-content-checkout">
+                        <?php echo do_shortcode('[woocommerce_checkout]'); ?>
                     </div>
                 </div>
             </div>
